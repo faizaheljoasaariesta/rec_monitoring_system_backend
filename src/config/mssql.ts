4c75b35c93b37a9fe95a1b0cdc85a9d68c00b257
Faizahel Joasa Ariesta <faizaheljoasaariesta@gmail.com>
2025-11-19 15:50:49 +0800

    @Faizahel try-1 windows connections to mssql

--- src/config/mssql.ts
+++ src/config/mssql.ts
@@ -1,19 +1,23 @@
-import { 
-  SERVER_NAME,
-  DATABASE_NAME,
-  USERNAME,
-  PASSWORD
-} from './env';
+// import { 
+//   SERVER_NAME,
+//   DATABASE_NAME,
+//   USERNAME,
+//   PASSWORD
+// } from './env';
 
 const mssql_config = {
-  server: SERVER_NAME,
-  database: DATABASE_NAME,
-  user: USERNAME,
-  password: PASSWORD,
+  server: '192.168.2.250',
+  database: 'REC_DB',
+  user: 'sa',
+  password: 'Rg54013657',
   options: {
-    encrypt: true,
+    encrypt: false,
     enableArithAbort: true,
     trustServerCertificate: true,
+    useUTC: true,
+    cryptoCredentialsDetails: {
+      minVersion: 'TLSv1'
+    }
   },
 
   connectionTimeout: 30000,  // 30 detik
